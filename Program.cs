37dea2d5ffe30847023b327d9ffa87277014a382
liquid72 <ricofitrap72@gmail.com>
2025-04-23 15:16:39 +0700

    Implement chat functionality with Azure OpenAI and add .env.sample for configuration

--- Program.cs
+++ Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using dotenv.net;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+// See https://aka.ms/new-console-template for more information
+DotEnv.Load();
+
+var builder = Kernel.CreateBuilder().AddAzureOpenAIChatCompletion(
+deploymentName: Environment.GetEnvironmentVariable("deploymentName") ?? string.Empty,
+    endpoint: Environment.GetEnvironmentVariable("endpoint") ?? string.Empty,
+    apiKey: Environment.GetEnvironmentVariable("apiKey") ?? string.Empty
+);
+
+
+Kernel kernel = builder.Build();
+var chatCompletionService = kernel.GetRequiredService<IChatCompletionService>();
+
+var history = new ChatHistory();
+string? userInput;
+
+do
+{
+    Console.WriteLine("Enter your message (or 'exit' to quit): ");
+    userInput = Console.ReadLine();
+    if (userInput == "exit") break;
+    history.AddUserMessage(userInput?? string.Empty);
+    var result = await chatCompletionService.GetChatMessageContentAsync(
+        history
+    );
+    Console.WriteLine($"AI: {result}");
+    history.AddAssistantMessage(result.Content.ToString());
+
+}
+while (true);
